28b2ad7ebca9153c9a0f27fb12b43558a1827bf5
Katharine Swilley <kswilley@gmail.com>
2022-06-19 20:45:12 -0700

    needs toString and tests

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
+const supertest = require('supertest');
 
 describe('Linked List', () => {
   it('works', () => {
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,67 @@
 'use strict';
 
-class LinkedList {
+const { link } = require('fs');
 
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
 }
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+
+  // add - from class demo
+  add(value){
+    const node = new Node(value);
+    if(!this.head){
+      this.head = node;
+      return;
+    }
+    let current = this.head;
+    while(current.next){
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  //insert
+  insert(value){
+    let newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  //includes - returns boolean
+  includes(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (this.current === value)
+        return true;
+    }
+    return false;
+  }
+
+  //to string - returns string representing all values
+  toString(){
+    let current = this.head;
+    let linkedListString = '';
+
+    return linkedListString;
+  }
+}
+
+
+let linkedList = new LinkedList();
+
+
+linkedList.add('a');
+linkedList.add('b');
+linkedList.add('c');
+linkedList.add('d');
 
 module.exports = LinkedList;

e5cf61757b5e3e9bdcb98c551d46a781991e89df
Katharine Swilley <kswilley@gmail.com>
2022-06-20 19:27:19 -0700

    initial commit for cc 6

--- javascript/linked-list/README.md
+++ javascript/linked-list/README.md
@@ -1,8 +1,13 @@
-# Singly Linked List
+# Singly Linked List/Linked List Insertions
 <!-- Short summary or background information -->
+
 
 ## Challenge
 <!-- Description of the challenge -->
+
+CC5 -
+
+CC6 - Extend a Linked List to allow various insertion methods.
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -2,15 +2,15 @@
 
 const { link } = require('fs');
 
-class Node{
-  constructor(value){
+class Node {
+  constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
 
 class LinkedList {
-  constructor(){
+  constructor() {
     this.head = null;
   }
 
@@ -24,22 +24,8 @@ class LinkedList {
     console.log('done traversing');
   }
 
-  // add
-  add(value){
-    const node = new Node(value);
-    if(!this.head){
-      this.head = node;
-      return;
-    }
-    let current = this.head;
-    while(current.next){
-      current = current.next;
-    }
-    current.next = node;
-  }
-
   //insert
-  insert(value){
+  insert(value) {
     let newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
@@ -61,19 +47,49 @@ class LinkedList {
   }
 
   //to string - returns string representing all values
-  toString(){
+  toString() {
     let current = this.head;
     let linkedListString = '';
 
-    while(current) {
+    while (current) {
       linkedListString += `{${current.value}} ->`;
       current = current.next;
     }
     linkedListString += 'NULL';
     return linkedListString;
   }
-}
 
+  // CC6 - add
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+
+  //CC6 - Insert Before - adds a new node with the given new value immediately before the first node that has the value specified
+
+  insertBefore(value, newValue) {
+    const current = this.head;
+    let newNode = new Node(newValue);
+
+  }
+
+  //CC6 - Insert After - adds a new node with the given new value immediately after the first node that has the value specified
+
+  insertAfter(value, newValue) {
+    let current = this.head;
+    let newNode = new Node(newValue);
+    
+  }
+}
 
 let linkedList = new LinkedList();
 

7bc52a044b4ab86fc3e6ecbdf7cb4c2f72c767af
Katharine Swilley <kswilley@gmail.com>
2022-07-31 18:12:06 -0700

    initial submit cc35

---
+++ javascript/graph/graphs.js
@@ -0,0 +1,116 @@
+'use strict';
+
+class Vertex {
+  constructor(value){
+    this.value = value;
+  }
+}
+
+class Edge {
+  constructor(vertex, weight = 0){
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor(){
+    this.adjacencyList = new Map();
+  }
+
+  addNode(value){
+    const node = new Vertex(value);
+    this.adjacencyList.set(node, []);
+    return node;
+  }
+
+  addEdge(startVertex, endVertex){
+    const neighbors = this.adjacencyList.get(startVertex);
+    neighbors.push(new Edge(endVertex));
+  }
+
+  getNodes(){
+    return [...this.adjacencyList];
+  }
+
+  getNeighbors(vertex){
+
+  }
+
+  size(){
+
+  }
+
+  //from demo
+  breadthFirst(root, cb){
+    const queue = [root];
+    const visited = new Set();
+    visited.add(root);
+    let current = null;
+
+    while (queue.length) {
+      current = queue.pop();
+
+      // do something if we want
+      if (cb) cb(current.value);
+
+      // grab neighbors?
+      const neighbors = this.getNeighbors(current);
+      for (let edge of neighbors) {
+        if (!visited.has(edge.vertex)) {
+          visited.add(edge.vertex);
+          queue.unshift(edge.vertex);
+        }
+      }
+    }
+
+    return visited;
+  }
+
+  depthFirst(root, cb){
+    const stack = [root];
+    const visited = new Set();
+    visited.add(root);
+    let current = null;
+
+    while (stack.length) {
+      current = stack.pop();
+
+      // do something if we want
+      if (cb) cb(current.value);
+
+      // grab neighbors?
+      const neighbors = this.getNeighbors(current);
+      for (let edge of neighbors) {
+        if (!visited.has(edge.vertex)) {
+          visited.add(edge.vertex);
+          stack.push(edge.vertex);
+        }
+      }
+    }
+
+    return visited;
+  }
+}
+
+//from demo
+const graph = new Graph();
+
+const A = graph.addVertex('A');
+const B = graph.addVertex('B');
+const D = graph.addVertex('D');
+const C = graph.addVertex('C');
+const E = graph.addVertex('E');
+const F = graph.addVertex('F');
+const G = graph.addVertex('G');
+const H = graph.addVertex('H');
+
+graph.addDirectedEdge(A, B);
+graph.addDirectedEdge(A, D);
+graph.addDirectedEdge(A, C);
+graph.addDirectedEdge(B, G);
+graph.addDirectedEdge(D, F);
+graph.addDirectedEdge(D, H);
+graph.addDirectedEdge(F, H);
+graph.addDirectedEdge(C, H);
+graph.addDirectedEdge(F, E);
